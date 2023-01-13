19bc032926b2f1c85bc87daa1485edb40bd3483f
Luis Sevilla <user.sevilu6@gmail.com>
2023-01-13 10:27:01 -0600

    added css, removed unnecessary files

---
+++ react-task-tracker/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='header'>
+        <h1>Task Tracker</h1>
+        <button className='btn'>Add</button>
+    </header>
+  )
+}
+
+export default Header
