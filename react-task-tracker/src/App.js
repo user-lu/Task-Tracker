2c4cf23ce2fc30448b97f8c5d238111e4cef8f18
Luis Sevilla <user.sevilu6@gmail.com>
2023-01-15 19:38:44 -0600

    worked on add button, added data to tasks, added delete function

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,9 +1,42 @@
+import React, { useState } from 'react'
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: 'true',
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: 'true',
+    },
+    {
+      id: 3,
+      text: 'Food Shooping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: 'false',
+    },
+  ])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+        : 'No Task to Show!'}
     </div>
   );
 }
--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Button from './Button'
 
 const Header = () => {
+  const onClick = () => {
+    console.log('Click');
+  }
+
   return (
     <header className='header'>
-        <h1>Task Tracker</h1>
-        <button className='btn'>Add</button>
+      <h1>Task Tracker</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
   )
 }
