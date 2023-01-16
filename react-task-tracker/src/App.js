f28d7d73b634a03d732c3b3daffb0229fa07a6f8
Luis Sevilla <user.sevilu6@gmail.com>
2023-01-16 05:52:57 -0600

    added AddTask component, worked on adding/deleting task functions

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from './components/AddTask';
 
-function App() {
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
-      text: 'Doctors appointment',
+      text: 'Doctors Appointment',
       day: 'Feb 5th at 2:30pm',
       reminder: 'true',
     },
@@ -18,11 +20,17 @@ function App() {
     },
     {
       id: 3,
-      text: 'Food Shooping',
+      text: 'Food Shopping',
       day: 'Feb 5th at 2:30pm',
       reminder: 'false',
     },
   ])
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task } 
+    setTasks([...tasks, newTask])
+  }
 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -39,7 +47,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
         : 'No Task to Show!'}
     </div>
