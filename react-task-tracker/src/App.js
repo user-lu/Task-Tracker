1246d0716dafbaf6d4c48800a4ccf1dae5456496
Luis Sevilla <user.sevilu6@gmail.com>
2023-01-18 12:44:29 -0600

    removed mock json server, will add actual backend later

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -6,36 +6,39 @@ import AddTask from './components/AddTask';
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: 'true',
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm',
-      reminder: 'true',
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder: 'false',
-    },
-  ])
+      {
+        "id": 1,
+        "text": "Doctors Appointment",
+        "day": "Feb 5th at 2:30pm",
+        "reminder": true
+      },
+      {
+        "id": 2,
+        "text": "Meeting at School",
+        "day": "Feb 6th at 1:30pm",
+        "reminder": true
+      },
+      {
+        "id": 3,
+        "text": "Food Shopping",
+        "day": "Feb 5th at 2:30pm",
+        "reminder": false
+      }
+    ])
 
+//Add Task
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
     const newTask = { id, ...task } 
     setTasks([...tasks, newTask])
   }
 
+//Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+//Toggle Reminder  
   const toggleReminder = (id) => {
     setTasks(
       tasks.map((task) =>
